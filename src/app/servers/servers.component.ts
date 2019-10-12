0642542e05277618937a9deea59833ed431a4a1f
DMLN <dml.nguyen@protonmail.com>
2019-10-12 15:54:53 -0400

    tutorial-1

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'servers',
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <server></server>
+  //   <server></server>
+  //   <server></server>
+  //   <server></server>
+  //   <server></server>
+  //   <server></server>
+  // `,
+  styleUrls: ['./servers.component.sass']
+})
+export class ServersComponent {
+
+}
